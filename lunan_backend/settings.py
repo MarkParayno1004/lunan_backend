02cd08f72f9375b0a3ec1ece5e42fea204df278b
MarkParayno1004 <paraynomarkphilip@gmail.com>
2026-03-02 08:22:16 +0800

    Config for Deploying to render

--- lunan_backend/settings.py
+++ lunan_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*m-4qh^4v5hq$)j1ft%02w03yyqz$6l4ohvjxm^vd&t&xnhao&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -146,7 +146,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
