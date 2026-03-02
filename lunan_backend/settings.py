0a5b0974d407c461bd26870d996939c672af9f20
MarkParayno1004 <paraynomarkphilip@gmail.com>
2026-03-02 08:26:05 +0800

    Fix Config

--- lunan_backend/settings.py
+++ lunan_backend/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +28,10 @@ SECRET_KEY = 'django-insecure-*m-4qh^4v5hq$)j1ft%02w03yyqz$6l4ohvjxm^vd&t&xnhao&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = [
+    '.onrender.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
