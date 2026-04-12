4011ea760cb7666e4a79f82dcba045b9e0cc3ddc
MarkParayno1004 <paraynomarkphilip@gmail.com>
2026-04-13 02:29:06 +0800

    Add twilio and add env

--- lunan_backend/settings.py
+++ lunan_backend/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 
